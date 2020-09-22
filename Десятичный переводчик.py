049534fa28d59bb2e6c0a0cb7c54b39d9f52df44
Никита Шаганский <nik.tos1510@gmail.com>
2020-09-22 20:03:02 +0300

    Change name of variables

--- Десятичный переводчик.py
+++ Десятичный переводчик.py
@@ -2,12 +2,12 @@ def decimal_translator(number, base):
     for i in str(number):
         if int(i) >= base:
             return None
-    nums = []
+    numbers = []
     for i in str(number):
-        nums.append(int(i))
-    nums.reverse()
+        numbers.append(int(i))
+    numbers.reverse()
     answer = 0
-    for i in range(len(nums)):
-        answer += base ** i * nums[i]
+    for i in range(len(numbers)):
+        answer += base ** i * numbers[i]
     return answer
 print(decimal_translator(10, 2))
